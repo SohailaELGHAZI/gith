71c4bf5ab72516486f63af1643aa77b3da742496
sohaila <sohailaelghazi@gmail.com>
2023-02-22 20:25:14 +0100

    code

---
+++ src/main/java/univ/fac/master/GithApplication.java
@@ -0,0 +1,13 @@
+package univ.fac.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithApplication.class, args);
+	}
+
+}
---
+++ src/main/java/univ/fac/master/controller/Controller.java
@@ -0,0 +1,12 @@
+package univ.fac.master.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/")
+public String Message() {
+	return "Welcome";
+}
+}
